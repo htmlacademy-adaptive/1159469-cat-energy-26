93e6360d4f5af13d1b8671f6d4c316b981314612
FR3NZY <manpatbro@gmail.com>
2022-08-21 23:30:56 +0300

    добавляет ретинизацию изображений на странице каталога

--- source/js/slider.js
+++ source/js/slider.js
@@ -3,9 +3,7 @@ let catAfter = document.querySelector('.slider__gallery--after');
 let sliderWrapper = document.querySelector('.slider__wrapper');
 
 sliderWrapper.onmousedown = function(event) {
-  console.log(event.offsetX);
   let x = event.offsetX;
-
   sliderControl.style.left = x + 'px';
   catAfter.style.clipPath = 'polygon(' + x + 'px 0%, 100% 0%, 100% 100%, ' + x + 'px 100%)';
 }
