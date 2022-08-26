a70372ac0d4bd0b47343c81c0aeccbc71500c56a
FR3NZY <manpatbro@gmail.com>
2022-08-26 15:26:13 +0300

    правки по замечаниям к 8 разделу

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,7 @@ import terser from 'gulp-terser';
 import squoosh from 'gulp-libsquoosh';
 import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
-import {deleteAsync} from 'del';
-
-// Styles
+import { deleteAsync } from 'del';
 
 export const styles = () => {
   return gulp.src('source/sass/style.scss', { sourcemaps: true })
@@ -28,23 +26,17 @@ export const styles = () => {
     .pipe(browser.stream());
 }
 
-// HTML
-
 const html = () => {
   return gulp.src('source/*.html')
   .pipe(htmlmin({ collapseWhitespace: true }))
   .pipe(gulp.dest('build'));
 }
 
-// Scripts
-
 const scripts = () => {
   return gulp.src('source/js/*.js')
   .pipe(terser())
   .pipe(gulp.dest('build/js'));
 }
-
-//Images
 
 const optimizeImages = () => {
   return gulp.src('source/img/**/*.{jpg,png}')
@@ -57,8 +49,6 @@ const copyImages = () => {
   .pipe(gulp.dest('build/img'));
 }
 
-// Webp
-
 const createWebp = () => {
   return gulp.src(['source/img/**/*.{jpg,png}', '!source/img/favicons/*'])
   .pipe(squoosh({
@@ -66,8 +56,6 @@ const createWebp = () => {
   }))
   .pipe(gulp.dest('build/img'));
 }
-
-// SVG
 
 const svg = () => {
   return gulp.src(['source/img/**/*.svg', '!source/img/sprite-icons/*.svg'])
@@ -85,8 +73,6 @@ export const createSprite = () => {
   .pipe(gulp.dest('build/img'));
 }
 
-// Copy
-
 const copy = (done) => {
   return gulp.src([
     'source/fonts/*.{woff,woff2}',
@@ -98,13 +84,9 @@ const copy = (done) => {
   .pipe(gulp.dest('build'));
 }
 
-// Clean
-
 const clean = () => {
   return deleteAsync('build');
 }
-
-// Server
 
 const server = (done) => {
   browser.init({
@@ -118,22 +100,16 @@ const server = (done) => {
   done();
 }
 
-//Reload
-
 const reload = (done) => {
   browser.reload
   done();
 }
-
-// Watcher
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/js/*.js', gulp.series(scripts));
   gulp.watch('source/*.html', gulp.series(html, reload));
 }
-
-//Build
 
 export const build = gulp.series(
   clean,
