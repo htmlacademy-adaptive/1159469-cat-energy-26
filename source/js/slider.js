ff15b37c6eeb42e5cb3b453e1f19c35146181561
FR3NZY <manpatbro@gmail.com>
2022-09-01 03:46:25 +0300

    fix: вносит правки по комментариям к 9 разделу

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,9 +1,11 @@
 let sliderControl = document.querySelector('.slider__control');
+let catBefore = document.querySelector('.slider__gallery--before');
 let catAfter = document.querySelector('.slider__gallery--after');
 let sliderWrapper = document.querySelector('.slider__wrapper');
 
 sliderWrapper.onmousedown = function(event) {
   let x = event.offsetX;
   sliderControl.style.left = x + 'px';
-  catAfter.style.clipPath = 'polygon(' + x + 'px 0%, 100% 0%, 100% 100%, ' + x + 'px 100%)';
+  catAfter.style.clipPath = 'polygon(' + x + 'px 0, 100% 0, 100% 100%, ' + x + 'px 100%)';
+  catBefore.style.clipPath = 'polygon(0 0, ' + x + 'px 0, ' + x + 'px 100%, 0 100%)';
 }
