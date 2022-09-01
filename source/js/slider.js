c8bd786835535f4b6ea105fba66d85b051c02870
FR3NZY <manpatbro@gmail.com>
2022-09-01 21:15:16 +0300

    2-ая попытка сделать работающий слайдер на JS =/

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,10 +2,35 @@ let sliderControl = document.querySelector('.slider__control');
 let catBefore = document.querySelector('.slider__gallery--before');
 let catAfter = document.querySelector('.slider__gallery--after');
 let sliderWrapper = document.querySelector('.slider__wrapper');
+let sliderBlock = document.querySelector('.results__slider');
 
-sliderWrapper.onmousedown = function(event) {
-  let x = event.offsetX;
-  sliderControl.style.left = x + 'px';
-  catAfter.style.clipPath = 'polygon(' + x + 'px 0, 100% 0, 100% 100%, ' + x + 'px 100%)';
-  catBefore.style.clipPath = 'polygon(0 0, ' + x + 'px 0, ' + x + 'px 100%, 0 100%)';
-}
+sliderControl.onmousedown = function(event) {
+
+  sliderBlock.appendChild(sliderControl);
+  sliderControl.style.top = event.pageY - (sliderControl.style.height / 2) + 'px';
+  sliderControl.style.transform = 'translateY(-50%)';
+  sliderControl.style.height = sliderWrapper.clientHeight + 'px';
+
+  function moveAt(event) {
+    sliderControl.style.left = event.pageX + 'px';
+  };
+
+  sliderWrapper.onmousemove = function(event) {
+    moveAt(event);
+    let x = event.offsetX;
+    catAfter.style.clipPath = 'polygon(' + x + 'px 0, 100% 0, 100% 100%, ' + x + 'px 100%)';
+    catBefore.style.clipPath = 'polygon(0 0, ' + x + 'px 0, ' + x + 'px 100%, 0 100%)';
+  };
+
+  sliderControl.onmouseup = function() {
+    sliderControl.onmouseup = null;
+  };
+
+  document.onmouseup = function() {
+    sliderWrapper.onmousemove = null;
+  };
+};
+
+sliderControl.ondragstart = function() {
+  return false;
+};
